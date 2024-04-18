8144090fccfafae40e8420674fceca2e3f23ee25
Tomas Zelaschi <totozelaschi@gmail.com>
2024-04-18 16:05:09 -0300

    [RED] crearAdministrador

---
+++ BusinessLogic/BusinessLogicTest/AdministradorTest.cs
@@ -0,0 +1,15 @@
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class AdministradorTest
+    {
+        private Administrador admin;
+
+        [TestMethod]
+        public void crearAdministrador()
+        {
+            Administrador admin = new Administrador();
+        }
+    }
+}
