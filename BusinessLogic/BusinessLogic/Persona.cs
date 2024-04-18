516ff4ddb4ef01522275069e1486323b410a677f
Tomas Zelaschi <totozelaschi@gmail.com>
2024-04-18 17:47:03 -0300

    [GREEN] testFromatoEmailIncorrecto

--- BusinessLogic/BusinessLogic/Persona.cs
+++ BusinessLogic/BusinessLogic/Persona.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace BusinessLogic
 {
@@ -7,7 +8,19 @@ namespace BusinessLogic
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public string Mail { get ; set; }
+
+        private string _mail;
+        public string Mail { get { return _mail; } 
+            set{
+                string pattern = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+                if (Regex.IsMatch(value, pattern)){
+                    _mail = value;
+                }
+                else {
+                    throw new ArgumentException("El formato del correo electrónico no es válido.");
+                }
+            }
+        }
         public string Password { get; set; }
 
         public Persona(int id, string nombre, string apellido, string mail, string password)
