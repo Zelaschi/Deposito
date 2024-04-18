751e6839394a54d2aba0b0101a830f94b1167c0b
Pedro Azambuja <pedroaza02@hotmail.com>
2024-04-18 17:07:46 -0300

    [RED] TestInitialize Promocion]

---
+++ BusinessLogic/BusinessLogicTest/TestPromocion.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogic;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    public class TestPromocion
+    {
+        private Promocion promocion;
+
+        [TestInitialize]
+        public void initialize()
+        {
+            promocion = new Promocion("promocion abril", 20, DateTime.Now, DateTime.Now.AddDays(10));
+        }
+    }
+}
