1122fef3999069b9533073d2e1e468a70bb7dda8
Tomas Zelaschi <totozelaschi@gmail.com>
2024-04-20 14:50:34 -0300

    [RED] crearClienteConParametrosTest

--- BusinessLogic/BusinessLogicTest/UsuarioTest.cs
+++ BusinessLogic/BusinessLogicTest/UsuarioTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicTest
 {
+    [TestClass]
     internal class UsuarioTest
     {
+        private Cliente cliente;
+        [TestInitialize]
+        public void crearUsuarioConParametrosTest() {
+            var idTest = 0;
+            var nombreTest = "Pedro";
+            var apellidoTest = "Azambuja";
+            var mailTest = "pedroazambuja@gmail.com";
+            var passwordTest = "Password1!";
+
+            cliente = new Cliente(idTest, nombreTest, apellidoTest, mailTest, passwordTest);
+        }
     }
 }
