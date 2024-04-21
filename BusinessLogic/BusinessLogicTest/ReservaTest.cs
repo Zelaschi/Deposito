a0bb83b9e01ee3d2c395398dd746a5a133e1b32e
Pedro Azambuja <pedroaza02@hotmail.com>
2024-04-21 16:44:55 -0300

    [RED] Reserva_Debe_Inicializarse_Con_Valores_Correctos

--- BusinessLogic/BusinessLogicTest/ReservaTest.cs
+++ BusinessLogic/BusinessLogicTest/ReservaTest.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessLogic;
 
 namespace BusinessLogicTest
 {
-    internal class ReservaTest
+    [TestClass]
+    public class ReservaTest
     {
+        [TestMethod]
+        public void Reserva_Debe_Inicializarse_Con_Valores_Correctos()
+        {
+            var deposito = new Deposito("A", "Mediano", true, 1);
+            var reserva = new Reserva(1, DateTime.Today, DateTime.Today.addDays(1), deposito, 100);
+
+            Assert.AreEqual(1, reserva.Id);
+            Assert.AreEqual(DateTime.Today, reserva.FechaDesde);
+            Assert.AreEqual(DateTime.Today.AddDays(1), reserva.FechaHasta);
+            Assert.AreEqual(deposito, reserva.Deposito);
+            Assert.AreEqual(100, reserva.precio);
+            Assert.AreEqual("Pendiente", reserva.Estado);
+        }
     }
 }
