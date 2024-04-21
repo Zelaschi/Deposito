e4b87d834ea8258cfb9502b1373553d78ee00c8b
Pedro Azambuja <pedroaza02@hotmail.com>
2024-04-21 17:18:28 -0300

    [GREEN] Reserva_Debe_Inicializarse_Con_Valores_Correctos

--- BusinessLogic/BusinessLogic/Reserva.cs
+++ BusinessLogic/BusinessLogic/Reserva.cs
@@ -4,5 +4,21 @@ namespace BusinessLogic
 {
     public class Reserva
     {
+        public static int UltimoID { get; set; } = 0;
+        public int Id { get; set; }
+        public DateTime FechaDesde { get; set; }
+        public DateTime FechaHasta { get; set; }
+        public Deposito Deposito { get; set; }  
+        public int Precio { get; set; }
+        public String Estado { get; set; } = "Pendiente";
+
+        public Reserva(DateTime fechaDesde, DateTime fechaHasta, Deposito deposito, int precio)
+        {
+            Id = ++UltimoID;
+            FechaDesde = fechaDesde;
+            FechaHasta = fechaHasta;
+            Deposito = deposito;
+            Precio = precio;
+        } 
     }
 }
--- BusinessLogic/BusinessLogicTest/ReservaTest.cs
+++ BusinessLogic/BusinessLogicTest/ReservaTest.cs
@@ -8,14 +8,13 @@ namespace BusinessLogicTest
         [TestMethod]
         public void Reserva_Debe_Inicializarse_Con_Valores_Correctos()
         {
-            var deposito = new Deposito("A", "Mediano", true, 1);
-            var reserva = new Reserva(1, DateTime.Today, DateTime.Today.addDays(1), deposito, 100);
+            var deposito = new Deposito("A", "Mediano", true);
+            Reserva reserva = new Reserva(DateTime.Today, DateTime.Today.AddDays(1), deposito, 100);
 
-            Assert.AreEqual(1, reserva.Id);
             Assert.AreEqual(DateTime.Today, reserva.FechaDesde);
             Assert.AreEqual(DateTime.Today.AddDays(1), reserva.FechaHasta);
             Assert.AreEqual(deposito, reserva.Deposito);
-            Assert.AreEqual(100, reserva.precio);
+            Assert.AreEqual(100, reserva.Precio);
             Assert.AreEqual("Pendiente", reserva.Estado);
         }
     }
