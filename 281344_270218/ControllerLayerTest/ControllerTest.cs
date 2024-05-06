b59a10967f412c9e4a6133b0bc8d1f69613c7947
Tomas Zelaschi <totozelaschi@gmail.com>
2024-05-06 19:42:13 -0300

    [RED] ObtenerListaReservasPendientesTest

--- 281344_270218/ControllerLayerTest/ControllerTest.cs
+++ 281344_270218/ControllerLayerTest/ControllerTest.cs
@@ -588,6 +588,32 @@ namespace ControllerLayerTest
             DTOReserva DTOReservaEncontrado = _controller.BuscarReservaPorId(aDTOReserva.Id);
 
             Assert.AreEqual(DTOReservaEncontrado.Estado, "Rechazada");
+        }
+        [TestMethod]
+        public void ObtenerListaReservasPendientesTest() 
+        {
+            _controller.RegistrarCliente(aDTOCliente);
+            _controller.RegistrarDeposito(aDTODeposito);
+
+            DTOReserva aDTOReserva1 = new DTOReserva(2, DateTime.Today.AddDays(10), DateTime.Today.AddDays(11), aDTODeposito, aDTOCliente);
+            DTOReserva aDTOReserva2 = new DTOReserva(2, DateTime.Today.AddDays(10), DateTime.Today.AddDays(15), aDTODeposito, aDTOCliente);
+            DTOReserva aDTOReserva3 = new DTOReserva(3, DateTime.Today.AddDays(10), DateTime.Today.AddDays(19), aDTODeposito, aDTOCliente);
+            DTOReserva aDTOReserva4 = new DTOReserva(4, DateTime.Today.AddDays(7), DateTime.Today.AddDays(14), aDTODeposito, aDTOCliente);
+
+            _controller.RegistrarReserva(aDTOReserva1);
+            _controller.RegistrarReserva(aDTOReserva2);
+            _controller.RegistrarReserva(aDTOReserva3);
+            _controller.RegistrarReserva(aDTOReserva4);
+
+            _controller.AceptarReserva(aDTOReserva1);
+            _controller.RechazarReserva(aDTOReserva2);
+
+            IList<DTOReserva> ReservasPendientes = _controller.ObtenerListaReservasPendientes();
+
+            DTOReserva reservaQueNoDeberiaEstar = ReservasPendientes.FirstOrDefault(x => x.Id == aDTOReserva1.Id);
+            Assert.AreEqual(aDTOReserva.FechaHasta, ReservasPendientes.FirstOrDefault(x => x.Id == aDTOReserva3.Id).FechaHasta);
+            Assert.AreEqual(aDTOReserva2.FechaHasta, ReservasPendientes.FirstOrDefault(x => x.Id == aDTOReserva4.Id).FechaHasta);
+            Assert.IsNull(reservaQueNoDeberiaEstar);
 
         }
 
