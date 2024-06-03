a79f0e4e530c283d1f33886598d37fa611e26620
Pedro Azambuja <pedroaza02@hotmail.com>
2024-06-03 16:36:06 -0300

    creacion interfaz reporte de reservas

---
+++ 281344_270218/BusinessLogic/ExportadorTXT.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class ExportadorTXT : IExportador
+    {
+        public string Exportar(IList<Reserva> reservas)
+        {
+            var reporte = new StringBuilder();
+            reporte.AppendLine("DEPOSITO\tRESERVA\tPAGO");
+            foreach (var reserva in reservas)
+            {
+                //CAMBIAR POR LOS DATOS QUE TENEMOS QUE MOSTRAR REALMENTE
+                reporte.AppendLine($"{reserva.Deposito.DepositoId}\t{reserva.ReservaId}\t{reserva.Estado}");
+            }
+            return reporte.ToString();
+        }
+    }
+}
