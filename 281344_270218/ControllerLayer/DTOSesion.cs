d5b0152e1c5d41112a53073432d7c3807fff7e55
Tomas Zelaschi <totozelaschi@gmail.com>
2024-06-05 18:12:57 -0300

    Cuando se pierde conexion y vuele que te mande a /

--- 281344_270218/ControllerLayer/DTOSesion.cs
+++ 281344_270218/ControllerLayer/DTOSesion.cs
@@ -4,6 +4,7 @@ namespace ControllerLayer
 {
     public class DTOSesion
     {
+        public bool logIn { get; set; }
         public string SesionMail { get; set; }
         public bool esAdministrador { get; set; } = false;
     }
